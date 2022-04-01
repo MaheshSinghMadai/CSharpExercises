ccfaca42417d456f930ae2e30303fde22132273d
corion <corion.pradhan@nestrait.com>
2022-04-01 14:19:33 +0545

    Four more solutions

---
+++ AliceBob/Program.cs
@@ -0,0 +1,66 @@
+﻿using AliceBob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AliceBob
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'compareTriplets' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER_ARRAY b
+         */
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int rewardAlice = 0;
+            int rewardBob = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    rewardAlice += 1;
+                }
+                else if (b[i] > a[i])
+                {
+                    rewardBob += 1;
+                }
+                else
+                {
+
+                }
+            }
+            var comparisonPoint = new List<int>() { rewardAlice, rewardBob };
+
+            return comparisonPoint; ;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+}
---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'diagonalDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int LeftToRightDiagonalSum = 0;
+            //int RightToLeftDiagonalSum = 0;
+
+            for(int i = 0; i < arr.Count(); i++)
+            {
+                LeftToRightDiagonalSum += arr[i][i];
+            }
+
+            //int diagonalDifference = LeftToRightDiagonal - RightToLeftDiagonal;
+
+            return LeftToRightDiagonalSum;
+
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Result.diagonalDifference(arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
